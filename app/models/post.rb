10add3e4db58373b216468765f6ecb279b64f2ff
竹本廉 <takemotoren@takemotorennoMacBook-Air.local>
2022-07-06 02:41:13 +0900

    validate

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,11 @@ class Post < ApplicationRecord
     validates :schedule, presence: true
     validates :start, presence: true
     validates :end, presence: true
+    validate :date_check
+
+    def date_check
+        errors.add(:end,"は開始日より後の日付を選択してください")
+        if self.start > self.end
+        end
+    end
 end
